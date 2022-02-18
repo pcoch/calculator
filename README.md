f8ad06f4ccee80177066d20dcd615be1f2a72ff3
paul <paulstuartcochrane@gmail.com>
2022-02-18 17:29:34 +1000

    Working Version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # calculator
 Odin Project: Calculator
+
+The calculator works with 4 main variables (firstNum, secondNum, operator, answer).
+
+firstNum is stored as a string when 
+
+The calculateAnswer function uses a Switch statment and uses the operator variable as the case. The operator variable is pulled from a click event on the operator buttons (i.e +, x, /, -), which takes the element ID (e.g 'add', 'subtract' etc).
+
+The main challenge was how to chain operations (i.e 4 x 5 + 6 - 2). At first, I was going to create an array stacking the numbers selected and stacking the operators by pushing onclick events into the arrays. That ended up being overkill.
+
+A much simpler solution was to store the answer variable after bum1 and num2 where stored, and then replace num1 with answer.
+
+//Scope & Functionality
+//when user clicks number it adds on top of the other number to create a new number called firstNum
+//when  user clicks an operator, the first number is stored and displayed
+//when user clicks number it adds on top of the other number to create a new number creating secondNum
+//when user clicks equals it operates and updates display value (calculate)
--- app.js
+++ app.js
@@ -1,50 +1,101 @@
-//Scope & Functionality
-//when user clicks number it adds on top of the other number to create a new number called firstNum
-//when  user clicks an operator, the first number is stored and displayed
-//when user clicks number it adds on top of the other number to create a new number creating secondNum
-//when user clicks equals it operates and updates display value (calculate)
+//global variables
+let screenValue = '';
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let currentAnswer = '';
 
-//variables
-let screenValue = ''
-let firstNum = ''
-let secondNum = ''
-let operator = ''
-
-const screen = document.querySelector('.calc-screen');
+//storing html elements
+const screen = document.querySelector('.calculator-screen');
 const numBtns = document.querySelectorAll('.num');
 const operatorBtns = document.querySelectorAll('.operator');
 const equalBtn = document.getElementById('equals');
 const clearBtn = document.getElementById('clear');
 const decimalBtn = document.getElementById('decimal');
 
-//when number is clicked
+//when button is clicked with mouse
 for (let i = 0; i < numBtns.length; i++) {
-    numBtns[i].addEventListener('click', (event) => {
+    numBtns[i].addEventListener('click', updateScreen)
+};
+
+function updateScreen(event) {
+    if (screenValue.length <= 10) {
 
         if (operator === "") {
-            screenValue = screenValue + event.target.innerText;
-            firstNum = screenValue;
-            screen.innerHTML = firstNum;
+            firstNum += event.target.innerText;
+            screenValue = firstNum;
+            screen.innerHTML = screenValue;
         }
 
         if (operator !== "") {
-            screenValue = screenValue + event.target.innerText;
-            secondNum = screenValue;
-            screen.innerHTML = secondNum;
+            secondNum += event.target.innerText;
+            screenValue = secondNum;
+            screen.innerHTML = screenValue;
         }
 
-        //only allow decimal click after a num
+        if (screenValue.includes('.') === true) {
+            decimalBtn.removeEventListener('click', updateScreen);
+        }
+    }
+};
 
-    })
-}
+// when num button is input with keypad
+document.addEventListener('keydown', (event) => {
+    keyUpdate(event)
+});
+
+function keyUpdate(event) {
+    let key
+    key = event.key;
+
+    if (screenValue.length <= 10) {
+
+        if (isFinite(event.key) === true) {
+
+            if (operator === "") {
+                firstNum += key;
+                screenValue = firstNum;
+                screen.innerHTML = screenValue;
+            }
+
+            if (operator !== "") {
+                secondNum += key;
+                screenValue = secondNum;
+                screen.innerHTML = screenValue;
+            }
+        }
+    }
+    //backspace functionality
+    if (event.key === 'Backspace' && operator === "") {
+        firstNum = screen.innerHTML.slice(0, -1);
+        screenValue = firstNum;
+        screen.innerHTML = screenValue;
+    }
+
+    if (event.key === 'Backspace' && operator !== "") {
+        secondNum = screen.innerHTML.slice(0, -1);
+        screenValue = secondNum;
+        screen.innerHTML = screenValue;
+    }
+};
 
 //when operator is clicked
 for (let i = 0; i < operatorBtns.length; i++) {
     operatorBtns[i].addEventListener('click', (event) => {
+
+        if (firstNum !== "" && secondNum !== "") {
+            calculateAnswer(firstNum, secondNum, operator)
+            firstNum = answer;
+            secondNum = "";
+            operator = "";
+        }
+
         screenValue = "";
         operator = event.target.getAttribute('id');
+        decimalBtn.addEventListener('click', updateScreen);
+
     })
-}
+};
 
 //when clear is clicked
 clearBtn.addEventListener('click', () => {
@@ -53,57 +104,48 @@ clearBtn.addEventListener('click', () => {
     operator = "";
     screenValue = "";
     screen.innerHTML = screenValue;
-})
+});
+
 
 //when equal button is clicked
-equalBtn.addEventListener('click', (num1, num2, op) => {
+equalBtn.addEventListener('click', () => {
+    calculateAnswer(firstNum, secondNum, operator);
+});
+
+//limiting answer to 5 decimal places
+function checkDecimal() {
+    if (Number.isInteger(answer) === true) {
+        screen.innerHTML = answer;
+    } else {
+        screen.innerHTML = answer.toFixed(2);
+    }
+};
+
+//calculation function
+function calculateAnswer(firstNum, secondNum, operator) {
     console.log(`firstnum is ${firstNum} secondNum is ${secondNum} operator is ${operator}`)
+
     switch (operator) {
         case "add":
-            screen.innerHTML = parseFloat(firstNum) + parseFloat(secondNum);
+            answer = parseFloat(firstNum) + parseFloat(secondNum);
+            checkDecimal()
             break;
 
         case "divide":
-            screen.innerHTML = parseFloat(firstNum) / parseFloat(secondNum);
+            if (secondNum == 0) {
+                screen.innerHTML = 'oi nah ☹️';
+            }
+            answer = parseFloat(firstNum) / parseFloat(secondNum);
+            checkDecimal()
             break;
 
         case "multiply":
-            screen.innerHTML = parseFloat(firstNum) * parseFloat(secondNum);
+            answer = parseFloat(firstNum) * parseFloat(secondNum);
+            checkDecimal()
             break;
 
         case "subtract":
-            screen.innerHTML = parseFloat(firstNum) - parseFloat(secondNum);
+            answer = parseFloat(firstNum) - parseFloat(secondNum);
+            checkDecimal()
     }
-})
-
-
-//old code
-// function updateDisplay(event) {
-// screenValue = screenValue + event.target.innerText;;
-// firstNum = screenValue;
-// screen.innerHTML = screenValue;
-
-// if (firstNum !== "") {
-//     screenValue = screenValue + event.target.innerText;;
-//     secondNum = screenValue;
-//     // console.log(secondNum);
-//     screen.innerHTML = screenValue;
-// }
-// }
-
-
-// function subtract(num1, num2) {
-//     return num1 - num2
-// }
-
-// function multiply(num1, num2) {
-//     return num1 * num2
-// }
-
-// function divide(num1, num2) {
-//     return num1 / num2
-// }
-
-// function percent(num1, num2) {
-//     return num1 % num2
-// }
+}
